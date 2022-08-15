2b0facc8fbfdd3303bb5b7105af77f09217cc7be
wisnurdi <wisnurdi@gmail.com>
2022-08-15 12:28:47 +0700

    add blog.js

---
+++ pages/blog.js
@@ -0,0 +1,12 @@
+
+
+export default function blog() {
+    return (
+      <Layout>
+        <h1 className="">Belajar Blog</h1>
+        <p>
+          Blog ini menyajikan berbagai artikel tutorial, digital marketing, web development, dan insight bisnis.
+        </p>
+      </Layout>
+    );
+  }
